6faac772f21563b3acde45e26c53fc553ed87368
Daiki UEDA <daikiueda.1979@gmail.com>
2011-08-17 02:39:18 +0900

    「Only variables should be passed by reference」の対応

--- htdocs/php-lib/obbligato/obbligato.php
+++ htdocs/php-lib/obbligato/obbligato.php
@@ -49,7 +49,7 @@ class Obbligato {
       }
 
       // キャッシュとして、ObbligatoFileDomのインスタンスを格納
-      $this->file_dom_cashes[ $my_file_path ] =& new ObbligatoFileDom( $this, $temp_dom, $my_file_path );
+      $this->file_dom_cashes[ $my_file_path ] = new ObbligatoFileDom( $this, $temp_dom, $my_file_path );
     }
     
     // ObbligatoFileDom型のデータを返す
@@ -249,7 +249,7 @@ class ObbligatoDom {
    * @param $my_dom Simple HTML DOM Parserで得られるDOM
    * @param $my_file_path ファイルパス
    */
-  public function __construct( &$my_controller = null, &$my_dom = null ){
+  public function __construct( $my_controller = null, $my_dom = null ){
     $this->controller =& $my_controller;
     $this->html_dom =& $my_dom;
   }
