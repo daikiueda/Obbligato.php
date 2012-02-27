909882f4904d1e33f48b8c33de25cfa1c8a220a7
Daiki UEDA <daikiueda.1979@gmail.com>
2012-02-27 15:11:28 +0900

    テンプレートファイルの指定機能、デフォルトの割り当て機能を実装

--- htdocs/php-lib/obbligato/config.php
+++ htdocs/php-lib/obbligato/config.php
@@ -11,6 +11,16 @@ define( "PATH_CONTENTS_ROOT_DIR", "" );
 /** Simple HTML DOM Parser ライブラリの場所 */
 define( "PATH_SIMPLE_HTML_DOM", "../simplehtmldom/simple_html_dom.php" );
 
+/** テンプレートファイルの格納ディレクトリ */
+define( "PATH_TEMPLATES_DIR", "./template" );
+
+/**
+ * デフォルトテンプレートのファイル名
+ * 設定しない場合は、空文字を記述
+ * （metaの指定が無い場合、テンプレートを適用しない）
+ */
+define( "STR_DEFAULT_TEMPLATE", "default.html" );
+
 /** キャッシュディレクトリ */
 define( "PATH_CACHE_DIR", "./cache" );
 ?>
--- htdocs/php-lib/obbligato/controller.php
+++ htdocs/php-lib/obbligato/controller.php
@@ -19,59 +19,79 @@ function init(){
 		exit;
 	}
 
-	//echo $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REDIRECT_URL"];
-	//$test_dom = file_get_html( $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REDIRECT_URL"] );
-	//foreach( $test_dom->find("meta") as $element ){
-	//  echo $element->outertext;
-	//}
-	// キャッシュファイルが存在し、最新の状態であれば、それを出力
-	if( false && is_file( PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"] ) ){
-		if(
-			( filemtime( $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REDIRECT_URL"] ) < filemtime( PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"] ) ) &&
-			( filemtime( "template/content.html" ) < filemtime( PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"] ) )
-		){
-			readfile( PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"] );
-			exit;
+	// リクエストされたHTMLファイルから、テンプレートのファイル名を取得
+	$template_file = "";
+	$test_dom = file_get_html( $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REDIRECT_URL"] );
+	$test_meta_elm = $test_dom->find( "meta[name=template]", 0 );
+	if( $test_meta_elm ){
+		$template_file = $test_meta_elm->content;
+	}
+	if( $template_file == "" ){
+		$template_file = STR_DEFAULT_TEMPLATE;
+	}
+	
+	// テンプレートの指定がない場合は、ファイルそのものを出力して処理を終了
+	if( $template_file == "" || $template_file == "NONE" ){
+		readfile( $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REDIRECT_URL"] );
+		exit;
+	}
+	
+	
+	// テンプレートの指定があり、テンプレートファイルが存在する場合は、
+	// 出力用HTMLの生成処理を実行
+	if( is_file( PATH_TEMPLATES_DIR . '/' . $template_file ) ){
+		
+		// キャッシュファイルが存在し、最新の状態であれば、それを出力
+		if( false && is_file( PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"] ) ){
+			if(
+				( filemtime( $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REDIRECT_URL"] ) < filemtime( PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"] ) ) &&
+				( filemtime( "template/content.html" ) < filemtime( PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"] ) )
+			){
+				readfile( PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"] );
+				exit;
+			}
 		}
+
+		// テンプレート埋め込み用の各種定数を設定
+		$page_uri = preg_replace( "/^" . str_replace( '/', '\\/', PATH_CONTENTS_ROOT_DIR ) . "/", "", $_SERVER["REDIRECT_URL"] );
+
+		/** 対象ページのURL */
+		define( "PAGE", $page_uri );
+
+		/** 対象ページが属する階層のインデックスページのURL */
+		define( "INDEX", preg_replace( "/\/[^\/]+$/", "/index.html", $page_uri ) );
+
+		/** 対象ページの上位階層のインデックスページのURL */
+		define( "PARENT", preg_replace( "/[^\/]+\/[^\/]+$/", "index.html", $page_uri ) );
+
+		/** コンテンツのルート階層のインデックスページのURL */
+		define( "ROOT", "/index.html" );
+
+
+		/** テンプレート埋め込み用オブジェクトを生成 */
+		$OBBLIGATO = new Obbligato( $page_uri );
+
+		// ページ構築
+		ob_start();
+		include PATH_TEMPLATES_DIR . '/' . $template_file;
+		$page_data = ob_get_contents();
+		ob_end_clean();
+
+		// キャッシュファイルを保存
+		// 該当ディレクトリが存在しない場合は、ディレクトリを新規作成
+		$target_dir = preg_replace( "/\/[^\/]+$/", "/", PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"] );
+		if( !is_dir( $target_dir ) ){
+			mkdir( $target_dir, 0777, true );
+		}
+		$cash_file_handle = fopen( PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"], "w+" );
+		fwrite( $cash_file_handle, $page_data );
+
+		// 出力
+		echo $page_data;
 	}
-
-	// テンプレート埋め込み用の各種定数を設定
-	$page_uri = preg_replace( "/^" . str_replace( '/', '\\/', PATH_CONTENTS_ROOT_DIR ) . "/", "", $_SERVER["REDIRECT_URL"] );
-
-	/** 対象ページのURL */
-	define( "PAGE", $page_uri );
-
-	/** 対象ページが属する階層のインデックスページのURL */
-	define( "INDEX", preg_replace( "/\/[^\/]+$/", "/index.html", $page_uri ) );
-
-	/** 対象ページの上位階層のインデックスページのURL */
-	define( "PARENT", preg_replace( "/[^\/]+\/[^\/]+$/", "index.html", $page_uri ) );
-
-	/** コンテンツのルート階層のインデックスページのURL */
-	define( "ROOT", "/index.html" );
-
-
-	/** テンプレート埋め込み用オブジェクト */
-	$OBBLIGATO = new Obbligato( $page_uri );
-
-
-	// ページ構築
-	ob_start();
-	include "template/content.html";
-	$page_data = ob_get_contents();
-	ob_end_clean();
-
-	// キャッシュファイルを保存
-	// 該当ディレクトリが存在しない場合は、ディレクトリを新規作成
-	$target_dir = preg_replace( "/\/[^\/]+$/", "/", PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"] );
-	if( !is_dir( $target_dir ) ){
-		mkdir( $target_dir, 0777, true );
+	else {
+		die( "No such template file : " . PATH_TEMPLATES_DIR . '/' . $template_file );
 	}
-	$cash_file_handle = fopen( PATH_CACHE_DIR . $_SERVER["REDIRECT_URL"], "w+" );
-	fwrite( $cash_file_handle, $page_data );
-
-	// 出力
-	echo $page_data;
 }
 
 init();
